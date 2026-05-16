a30d28d6850b5621f204817fd5c212108ca81dcf
alfonso818 <alfonsoguillera28@gmail.com>
2026-05-16 11:23:47 +0700

    pos-frontend V2 Commit 1

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,101 @@
-function App() {
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const API =
+  "URL_BACKEND_RAILWAY";
+
+export default function App() {
+
+  const [products, setProducts] = useState([]);
+
+  const [form, setForm] = useState({
+    name: "",
+    stock: "",
+    buy_price: "",
+    sell_price: ""
+  });
+
+  const getProducts = async () => {
+    const res = await axios.get(
+      `${API}/products`
+    );
+
+    setProducts(res.data);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const addProduct = async () => {
+    await axios.post(
+      `${API}/products`,
+      form
+    );
+
+    getProducts();
+  };
 
   return (
-    <div>
-      <h1>POS Frontend Running</h1>
+    <div style={{ padding: 20 }}>
+      <h1>POS APP</h1>
+
+      <input
+        placeholder="Nama"
+        onChange={(e)=>
+          setForm({
+            ...form,
+            name:e.target.value
+          })
+        }
+      />
+
+      <input
+        placeholder="Stock"
+        onChange={(e)=>
+          setForm({
+            ...form,
+            stock:e.target.value
+          })
+        }
+      />
+
+      <input
+        placeholder="Harga Beli"
+        onChange={(e)=>
+          setForm({
+            ...form,
+            buy_price:e.target.value
+          })
+        }
+      />
+
+      <input
+        placeholder="Harga Jual"
+        onChange={(e)=>
+          setForm({
+            ...form,
+            sell_price:e.target.value
+          })
+        }
+      />
+
+      <button onClick={addProduct}>
+        Tambah
+      </button>
+
+      <hr />
+
+      {
+        products.map((p)=>(
+          <div key={p.id}>
+            <h3>{p.name}</h3>
+            <p>Stock: {p.stock}</p>
+            <p>Harga: {p.sell_price}</p>
+          </div>
+        ))
+      }
+
     </div>
   );
-
 }
-
-export default App;
